3656d9cdb125085bd7663540373aa773b3050ca7
Easy-going12 <qntjf23@naver.com>
2025-09-22 19:48:28 +0900

    구조 및 DTO 작성

---
+++ CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/post/query/dto/categoryDTO.java
@@ -0,0 +1,9 @@
+package com.backoven.catdogshelter.domain.post.query.dto;
+
+public enum categoryDTO {
+    SPAM,
+    ABUSE,
+    PORN,
+    FRAUD,
+    MISINFO
+}
---
+++ CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/post/query/dto/postReportDTO.java
@@ -0,0 +1,8 @@
+package com.backoven.catdogshelter.domain.post.query.dto;
+
+public class postReportDTO {
+    private int id;
+    private categoryDTO category;
+    private String etc_detail;
+    private String created_at;
+}
