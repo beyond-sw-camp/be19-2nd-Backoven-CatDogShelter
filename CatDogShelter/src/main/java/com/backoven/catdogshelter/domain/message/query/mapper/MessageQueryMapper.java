d8cd7279e89c7962f0efc9c0b15ecf1ed2507969
dddd0ng <kdg1644@naver.com>
2025-09-23 10:13:33 +0900

    setup : 클래스명 변경, mapper.xml 추가

---
+++ CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/message/query/mapper/MessageQueryMapper.java
@@ -0,0 +1,8 @@
+package com.backoven.catdogshelter.domain.message.query.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MessageQueryMapper {
+
+}
---
+++ CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/sighting/command/domain/repository/SightingJpaRepositoryInterface.java
@@ -0,0 +1,9 @@
+//package com.backoven.catdogshelter.domain.sighting.command.domain.repository;
+//
+///* 설명. JPA를 위한 인터페이스(<테이블 entity, PK 타입>) */
+//
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface SightingJpaRepositoryInterface extends JpaRepository<Integer, Integer> {
+//}
