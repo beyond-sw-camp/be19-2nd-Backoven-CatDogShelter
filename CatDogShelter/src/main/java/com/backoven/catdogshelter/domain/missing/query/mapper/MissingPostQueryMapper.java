e545c9ecd3401e12ea3506a307fd0714e4408cb1
dddd0ng <kdg1644@naver.com>
2025-09-24 16:50:51 +0900

    feat: MissingPost 게시글 조회 기능 MyBatis 구현 완료

--- CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/missing/query/mapper/MissingPostQueryMapper.java
+++ CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/missing/query/mapper/MissingPostQueryMapper.java
@@ -11,17 +11,17 @@ import java.util.List;
 @Mapper
 public interface MissingPostQueryMapper {
     // REQ-021 게시글 목록 조회 (검색 + 페이징)
-    List<MissingPostQueryDTO> searchPosts(@Param("keyword") String keyword,
+    List<MissingPostQueryDTO> selectMissingPostSearch(@Param("keyword") String keyword,
                                           @Param("offset") int offset,
                                           @Param("limit") int limit);
 
     // REQ-029 조회수 기준 인기 게시글
-    List<MissingPostQueryDTO> findPopularPostsByView(@Param("limit") int limit);
+    List<MissingPostQueryDTO>   selectAllMissingPostsByView(@Param("limit") int limit);
     // REQ-030 추천수 기준 인기 게시글
-    List<MissingPostQueryDTO> findPopularPostsByLike(@Param("limit") int limit);
+    List<MissingPostQueryDTO> selectAllMissingPostsByLiked(@Param("limit") int limit);
 
     // REQ-031 최신 게시글 조회
-    List<MissingPostQueryDTO> findLatestPosts(@Param("limit") int limit);
+    List<MissingPostQueryDTO> selectAllMissingPostsLatest(@Param("limit") int limit);
 
     // REQ-028 조회수 증가
     void increaseView(@Param("id") Long id);
@@ -30,13 +30,13 @@ public interface MissingPostQueryMapper {
     MissingPostQueryDTO selectPostById(Long id);
 
     //게시판보드, 게시글 목록 조회 쿼리
-    List<MissingPostQueryDTO> findAllPostsWithLikeCount();
+    List<MissingPostQueryDTO> selectAllMissingPosts();
 
     //실종신고 게시글 상세내용 조회(게시글 하나 안에서)
-    MissingPostQueryDetailDTO findPostDetail(int postId);
+    MissingPostQueryDetailDTO selectMissingPostDetail(int postId);
 
     //댓글 목록 (페이징 처리)
-    List<MissingPostCommentDTO> findComments(@Param("postId") int postId,
+    List<MissingPostCommentDTO> selectMissingPostDetailComments(@Param("postId") int postId,
                                              @Param("offset") int offset,
                                              @Param("limit") int limit);
 
--- CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/missing/query/service/MissingPostQueryService.java
+++ CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/missing/query/service/MissingPostQueryService.java
@@ -14,38 +14,35 @@ import java.util.List;
 public class MissingPostQueryService {
 
     private final MissingPostQueryMapper mapper;
-    // 게시판보드, 게시글 목록 조회 쿼리
-    public List<MissingPostQueryDTO> getAllPostsWithLikeCount() {
-        return mapper.findAllPostsWithLikeCount();
-    }
-
     //REQ-021게시글 서치 조회
-    public List<MissingPostQueryDTO> searchPosts(String keyword, int page, int size) {
+
+    public List<MissingPostQueryDTO> selectMissingPostSearch(String keyword, int page, int size) {
         int offset = (page - 1) * size;
-        return mapper.searchPosts(keyword, offset, size);
+        return mapper.selectMissingPostSearch(keyword, offset, size);
     }
-
     //REQ-029 조회수 기준 인기글
-    public List<MissingPostQueryDTO> getPopularPostsByView(int limit) {
-        return mapper.findPopularPostsByView(limit);
-    }
 
+    public List<MissingPostQueryDTO> selectAllMissingPostsByView(int limit) {
+        return mapper.selectAllMissingPostsByView(limit);
+    }
     //REQ-030 추천수 기준 인기글
-    public List<MissingPostQueryDTO> getPopularPostsByLike(int limit) {
-        return mapper.findPopularPostsByLike(limit);
+
+    public List<MissingPostQueryDTO> selectAllMissingPostsByLiked(int limit) {
+        return mapper.selectAllMissingPostsByLiked(limit);
     }
 
     //REQ-031 최신 작성 게시글
-    public List<MissingPostQueryDTO> getLatestPosts(int limit) {
-        return mapper.findLatestPosts(limit);
-    }
 
+    public List<MissingPostQueryDTO> selectAllMissingPostsLatest(int limit) {
+        return mapper.selectAllMissingPostsLatest(limit);
+    }
     //조회수 증가 로직
+
     public void increaseView(Long id) {
         mapper.increaseView(id);
     }
-
     //REQ-028 게시글 조회 하면 조회 수 증가
+
     public MissingPostQueryDTO getPostById(Long id) {
         MissingPostQueryDTO dto = mapper.selectPostById(id);
         if (dto == null) {
@@ -54,16 +51,25 @@ public class MissingPostQueryService {
         return dto;
     }
 
+
+
+    // 게시판보드, 게시글 목록 조회 쿼리
+    public List<MissingPostQueryDTO> selectAllMissingPosts() {
+        return mapper.selectAllMissingPosts();
+    }
+
+
+
     // 실종신고 게시글 내용 상세 조회
-    public MissingPostQueryDetailDTO getPostDetail(int postId) {
-        return mapper.findPostDetail(postId);
+    public MissingPostQueryDetailDTO selectMissingPostDetail(int postId) {
+        return mapper.selectMissingPostDetail(postId);
 
     }
 
     //댓글 api처리
-    public List<MissingPostCommentDTO> getComments(int postId, int page, int size) {
+    public List<MissingPostCommentDTO> selectMissingPostDetailComments(int postId, int page, int size) {
         int offset = (page - 1) * size;
-        return mapper.findComments(postId, offset, size);
+        return mapper.selectMissingPostDetailComments(postId, offset, size);
     }
     //해당 게시글의 전체 댓글 수를 가져옴(페이징 정보를 클라이언트에 내려주기 위해)
     public int getTotalCount(int postId) {
