5a35c7f93d0d1d511ddadc98d91ddb26d5ffd15d
bynmch <byunmch@naver.com>
2025-09-25 22:10:22 +0900

    봉사모임 및 봉사후기 조회
    검색/목록/상세
    
    등급제외

---
+++ CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/volunteer/query/mapper/VolunteerPostQueryMapper.java
@@ -0,0 +1,22 @@
+package com.backoven.catdogshelter.domain.volunteer.query.mapper;
+
+import com.backoven.catdogshelter.domain.volunteer.query.dto.VolunteerPostCommentDTO;
+import com.backoven.catdogshelter.domain.volunteer.query.dto.VolunteerPostDTO;
+import com.backoven.catdogshelter.domain.volunteer.query.dto.VolunteerPostFileDTO;
+import com.backoven.catdogshelter.domain.volunteer.query.dto.VolunteerPostListDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface VolunteerPostQueryMapper {
+
+    List<VolunteerPostListDTO> selectVolunteerPostList();
+
+
+    List<VolunteerPostCommentDTO> selectVolunteerPostComments(int id);
+    List<VolunteerPostFileDTO> selectVolunteerPostFiles(int id);
+    VolunteerPostDTO selectVolunteerPost(int id);
+
+    List<VolunteerPostListDTO> selectVolunteerPostsListByKeyword(String keyword);
+}
