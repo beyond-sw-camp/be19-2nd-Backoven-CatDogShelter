e2c31c1dca284f098e1856200fd14003f8a8427d
bynmch <byunmch@naver.com>
2025-09-26 13:45:30 +0900

    공지사항 전체/검색/상세 조회

--- CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/notice/query/dto/NoticePostQueryDTO.java
+++ CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/notice/query/dto/NoticePostQueryDTO.java
@@ -3,9 +3,11 @@ package com.backoven.catdogshelter.domain.notice.query.dto;
 import lombok.Data;
 
 @Data
-public class NoticeQueryDTO {
+public class NoticePostQueryDTO {
     private int id;
     private String title;
-    private String createdAt;
+    private String content;
+    private String created;
+    private String updated;
     private String writer;
 }
--- CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/notice/query/service/NoticeQueryService.java
+++ CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/notice/query/service/NoticeQueryService.java
@@ -1,11 +1,13 @@
 package com.backoven.catdogshelter.domain.notice.query.service;
 
+import com.backoven.catdogshelter.domain.notice.query.dto.NoticePostQueryDTO;
 import com.backoven.catdogshelter.domain.notice.query.dto.NoticeQueryDTO;
 import com.backoven.catdogshelter.domain.notice.query.mapper.NoticeQueryMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class NoticeQueryService {
@@ -17,7 +19,18 @@ public class NoticeQueryService {
         this.noticeQueryMapper = noticeQueryMapper;
     }
 
+    // 공지사항 전체 목록 조회
     public List<NoticeQueryDTO> selectNotices() {
         return noticeQueryMapper.selectNotices();
+    }
+
+    // 공지사항 검색 목록 조회(제목 || 내용)
+    public List<NoticeQueryDTO> selectNoticesByKeyword(String keyword) {
+        return noticeQueryMapper.selectNoticesByKeyword(keyword);
+    }
+
+
+    public NoticePostQueryDTO selectNotice(int id) {
+        return noticeQueryMapper.selectNotice(id);
     }
 }
