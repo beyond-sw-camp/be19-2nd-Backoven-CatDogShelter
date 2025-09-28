a4d4a7dd31ad85c018059f6bf41a090a29b4188a
dddd0ng <117462826+dddd0ng@users.noreply.github.com>
2025-09-28 23:48:20 +0900

    물품기부 게시판 CUD 기능 JPA로 구현 완료 (#111)
    * db : insert dummy data
    
    * db : insert dummy data
    
    * db : insert dummy data
    
    * setup : 클래스명 변경, mapper.xml 추가
    
    * feat : Mabatis - MissingPost select
    
    * feat: MissingPost 게시글 조회 기능 MyBatis 구현 완료
    
    * feat: MissingPost 게시글 조회 기능 MyBatis 구현 완료
    
    * update : 실종신고 게시글 목록 조회 로직 수정
    
    * setup : DDL, dummyData
    
    * setup : DDL, dummyData
    
    * feat : Mybatis - DonationPost select
    
    * no message
    
    * update : 실종신고 게시글 목록 조회 로직 수정
    
    * update : post select할 때 user rating 표기
    
    * update : post select할 때 user rating 표기
    
    * 작업중인 변경사항 백업
    
    * Resolve merge conflicts with devlop
    
    * feat : 물품기부게시판 JPA 기능 구현
    
    * feat : 물품기부게시판 JPA 기능 구현
    
    * feat : 물품기부게시판 JPA 기능 구현
    
    * feat : 물품기부게시판 JPA 기능 구현
    
    * feat : 물품기부게시판 JPA 기능 구현
    
    * feat : 물품기부게시판 JPA 기능 구현
    
    * feat : 물품기부게시판 JPA 기능 구현
    
    * feat : 물품기부게시판 JPA 기능 구현

---
+++ CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/donation/command/domain/repository/DonationPostCommentReportRepository.java
@@ -0,0 +1,11 @@
+package com.backoven.catdogshelter.domain.donation.command.domain.repository;
+
+import com.backoven.catdogshelter.domain.donation.command.domain.aggregate.entity.DonationPost;
+import com.backoven.catdogshelter.domain.donation.command.domain.aggregate.entity.DonationPostCommentReport;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface DonationPostCommentReportRepository extends JpaRepository<DonationPostCommentReport, Long> {
+
+}
