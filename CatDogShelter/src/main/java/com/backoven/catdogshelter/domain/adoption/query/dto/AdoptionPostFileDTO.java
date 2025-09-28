1247d30fe0f98134e30e5939873d934ccf2175e7
haenin <143497104+haenin@users.noreply.github.com>
2025-09-28 18:58:08 +0900

    Feature/user (#99)
    * Update README.md
    
    * Update README.md (#33)
    
    * mybatis 등급 제외 select 완료
    
    # Conflicts:
    #	DB/DDL/CatDogShelter_DDL.sql
    #	README.md
    
    * adoption-post mybatis
    
    * adoption-post jpa 완료
    
    * user 토큰과 함께 개발 완료
    
    * user 토큰과 함께 개발 완료
    
    * user 토큰과 함께 개발 완료
    
    * user 토큰과 함께 개발 완료
    # Conflicts:
    #	CatDogShelter/.gitignore
    #	CatDogShelter/build.gradle
    #	CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/adoption/command/application/controller/AdoptionPostCommandController.java
    #	CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/adoption/command/application/dto/AdoptionPostCommandDTO.java
    #	CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/adoption/command/infrastructure/service/ExternalService.java
    #	CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/adoption/query/dto/AdoptionPostFileDTO.java
    #	CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/adoption/query/dynamic/SearchCriteria.java
    #	CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/adoption/query/mapper/AdoptionMapper.java
    #	CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/user/command/application/controller/UserController.java
    #	CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/user/command/application/service/UserService.java
    #	CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/user/command/application/service/UserServiceImpl.java
    #	CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/user/command/domain/repository/UserJpaRepositoryInterface.java
    #	CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/user/query/controller/QueryController.java
    #	CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/user/query/service/QueryService.java
    #	CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/user/security/AuthenticationFilter.java
    #	CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/user/security/CustomUserDetails.java
    #	CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/user/security/WebSecurity.java
    #	CatDogShelter/src/main/resources/application.yml
    #	CatDogShelter/src/main/resources/com/backoven/catdogshelter/domain/adoption/query/mapper/AdoptionMapper.xml
    #	DB/DDL/CatDogShelter_DDL.sql
    #	README.md

--- CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/adoption/query/dto/AdoptionPostFileDTO.java
+++ CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/adoption/query/dto/AdoptionPostFileDTO.java
@@ -11,7 +11,7 @@ public class AdoptionPostFileDTO {
     private String fileRename;   // 리네임된 파일명
     private String filePath;     // 파일 저장 경로
     private String uploadedAt;   // 업로드 시각
-
     // 이미지 조회 API URL
     private String fileUrl;
+
 }
