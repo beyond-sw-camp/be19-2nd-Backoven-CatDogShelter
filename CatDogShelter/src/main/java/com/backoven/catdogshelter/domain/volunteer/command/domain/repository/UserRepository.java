27c3af881af1e7b6ad1f90d259167971cb138101
Byun-Mincheol <150311355+bynmch@users.noreply.github.com>
2025-09-29 10:47:36 +0900

    봉사모임 파트 기능 구현 (#131)

---
+++ CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/volunteer/command/domain/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.backoven.catdogshelter.domain.volunteer.command.domain.repository;
+
+import com.backoven.catdogshelter.common.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+}
