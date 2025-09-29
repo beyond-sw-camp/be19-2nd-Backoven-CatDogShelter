d4607ab3c71e015462ee0c681686c66092735df8
LeeSeonggeon <tmdrjs0040@gmail.com>
2025-09-29 18:09:57 +0900

    no message

---
+++ CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/volunteer/command/domain/repository/ShelterHeadRepository.java
@@ -0,0 +1,7 @@
+//package com.backoven.catdogshelter.domain.volunteer.command.domain.repository;
+//
+//import com.backoven.catdogshelter.common.entity.ShelterHeadEntity;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface ShelterHeadRepository extends JpaRepository<ShelterHeadEntity, Long> {
+//}
---
+++ CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/volunteer/command/domain/repository/SigunguRepository.java
@@ -0,0 +1,7 @@
+//package com.backoven.catdogshelter.domain.volunteer.command.domain.repository;
+//
+//import com.backoven.catdogshelter.common.entity.SigunguEntity;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface SigunguRepository extends JpaRepository<SigunguEntity, Integer> {
+//}
