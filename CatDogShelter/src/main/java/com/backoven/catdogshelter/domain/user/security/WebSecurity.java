3fa1968ec7895c5c27160c08572aabf634a5c424
haenin <gpdnjsqhdld@naver.com>
2025-09-29 14:07:06 +0900

    user 개발 완료

--- CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/user/security/WebSecurity.java
+++ CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/user/security/WebSecurity.java
@@ -59,6 +59,7 @@ public class WebSecurity {
                         .requestMatchers(HttpMethod.GET, "/catdogshelter/user/**").authenticated() // 조회는 인증 필요
                         .requestMatchers(HttpMethod.PUT, "/catdogshelter/user/**").authenticated() // 수정도 인증 필요
                         .requestMatchers("/catdogshelter/admin/**").permitAll()
+                        .requestMatchers("/catdogshelter/**").permitAll()
                         .anyRequest().authenticated())
                 // 세션을 안쓰겟다
                 /* 설명. Session 방식이 아닌 JWT Token 방식을 사용하겠다. */
