205e84fc2f9af620143b89133aa079d395d58255
LeeSeonggeon <tmdrjs0040@gmail.com>
2025-09-29 17:29:33 +0900

    [스웨거 작성] 컨트롤 파트

--- CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/message/command/application/controller/AMessageController.java
+++ CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/message/command/application/controller/AMessageController.java
@@ -2,6 +2,8 @@ package com.backoven.catdogshelter.domain.message.command.application.controller
 
 import com.backoven.catdogshelter.domain.message.command.application.dto.RequestMessageDTO;
 import com.backoven.catdogshelter.domain.message.command.application.service.AMessageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,7 @@ import java.net.URI;
 
 @RestController
 @RequestMapping("/message")
+@Tag(name = "메시지 CUD API")
 public class AMessageController {
     private final AMessageService aMessageService;
 
@@ -21,6 +24,7 @@ public class AMessageController {
 
     // 쪽지 보내기
     @PostMapping("/message")
+    @Operation(summary = "메시지 보내기", description = "들어온 입력값을 토대로 메시지를 보냅니다.")
     public ResponseEntity<?> registMessage(@RequestBody RequestMessageDTO newMessageDTO) {
         int messageId = aMessageService.registMessage(newMessageDTO);
 
@@ -31,6 +35,7 @@ public class AMessageController {
 
     // 쪽지 삭제하기
     @DeleteMapping("/{messageId}")
+    @Operation(summary = "메시지 삭제하기", description = "입력값을 id로 가지는 메시지를 삭제합니다.")
     public ResponseEntity<?> deleteMessage(@PathVariable int messageId) {
         aMessageService.removeMessage(messageId);
 
