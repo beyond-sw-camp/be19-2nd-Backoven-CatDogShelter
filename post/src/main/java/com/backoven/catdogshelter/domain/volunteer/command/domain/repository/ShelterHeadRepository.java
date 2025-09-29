603310ebcac35b2cf62ed675b51ef7c1d43280a5
haenin <gpdnjsqhdld@naver.com>
2025-09-29 15:53:29 +0900

    user 개발 완료

--- post/src/main/java/com/backoven/catdogshelter/domain/volunteer/command/domain/repository/ShelterHeadRepository.java
+++
@@ -1,7 +0,0 @@
-package com.backoven.catdogshelter.domain.volunteer.command.domain.repository;
-
-import com.backoven.catdogshelter.common.entity.ShelterHeadEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShelterHeadRepository extends JpaRepository<ShelterHeadEntity, Long> {
-}
