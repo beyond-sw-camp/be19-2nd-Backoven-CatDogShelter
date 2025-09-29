75c83d0d6564da6c7e2d26aec7a2e09fdf563111
LeeSeonggeon <tmdrjs0040@gmail.com>
2025-09-29 18:09:57 +0900

    no message

--- post/src/main/java/com/backoven/catdogshelter/domain/volunteer/command/domain/repository/VolunteerShelterHeadRepository.java
+++ post/src/main/java/com/backoven/catdogshelter/domain/volunteer/command/domain/repository/VolunteerShelterHeadRepository.java
@@ -1,3 +1,4 @@
+<<<<<<<< HEAD:post/src/main/java/com/backoven/catdogshelter/domain/volunteer/command/domain/repository/VolunteerShelterHeadRepository.java
 package com.backoven.catdogshelter.domain.volunteer.command.domain.repository;
 
 import com.backoven.catdogshelter.common.entity.ShelterHeadEntity;
@@ -5,3 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VolunteerShelterHeadRepository extends JpaRepository<ShelterHeadEntity, Long> {
 }
+========
+//package com.backoven.catdogshelter.domain.volunteer.command.domain.repository;
+//
+//import com.backoven.catdogshelter.common.entity.ShelterHeadEntity;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface ShelterHeadRepository extends JpaRepository<ShelterHeadEntity, Long> {
+//}
+>>>>>>>> 7372e29 (no message):CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/volunteer/command/domain/repository/ShelterHeadRepository.java
--- userService/src/main/java/com/backoven/catdogshelter/domain/user/command/domain/repository/SigunguRepository.java
+++ userService/src/main/java/com/backoven/catdogshelter/domain/user/command/domain/repository/SigunguRepository.java
@@ -1,3 +1,4 @@
+<<<<<<<< HEAD:userService/src/main/java/com/backoven/catdogshelter/domain/user/command/domain/repository/SigunguRepository.java
 package com.backoven.catdogshelter.domain.user.command.domain.repository;
 
 import com.backoven.catdogshelter.common.entity.SigunguEntity;
@@ -5,3 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SigunguRepository extends JpaRepository<SigunguEntity, Integer> {
 }
+========
+//package com.backoven.catdogshelter.domain.volunteer.command.domain.repository;
+//
+//import com.backoven.catdogshelter.common.entity.SigunguEntity;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface SigunguRepository extends JpaRepository<SigunguEntity, Integer> {
+//}
+>>>>>>>> 7372e29 (no message):CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/volunteer/command/domain/repository/SigunguRepository.java
