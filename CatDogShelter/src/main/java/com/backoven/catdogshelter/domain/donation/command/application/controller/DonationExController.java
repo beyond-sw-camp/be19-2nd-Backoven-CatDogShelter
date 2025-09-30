51a7fe56d8537024663e57a646673b1ce0ca74a9
dddd0ng <117462826+dddd0ng@users.noreply.github.com>
2025-09-30 14:55:39 +0900

    Feature/swagger 기능 추가 (#167)
    * db : insert dummy data
    
    * db : insert dummy data
    
    * db : insert dummy data
    
    * setup : 클래스명 변경, mapper.xml 추가
    
    * feat : Mabatis - MissingPost select
    
    * feat: MissingPost 게시글 조회 기능 MyBatis 구현 완료
    
    * feat: MissingPost 게시글 조회 기능 MyBatis 구현 완료
    
    * update : 실종신고 게시글 목록 조회 로직 수정
    
    * setup : DDL, dummyData
    
    * setup : DDL, dummyData
    
    * feat : Mybatis - DonationPost select
    
    * no message
    
    * update : 실종신고 게시글 목록 조회 로직 수정
    
    * update : post select할 때 user rating 표기
    
    * update : post select할 때 user rating 표기
    
    * 작업중인 변경사항 백업
    
    * Resolve merge conflicts with devlop
    
    * feat : 물품기부게시판 JPA 기능 구현
    
    * feat : 물품기부게시판 JPA 기능 구현
    
    * feat : 물품기부게시판 JPA 기능 구현
    
    * feat : 물품기부게시판 JPA 기능 구현
    
    * feat : 물품기부게시판 JPA 기능 구현
    
    * feat : 물품기부게시판 JPA 기능 구현
    
    * feat : 물품기부게시판 JPA 기능 구현
    
    * feat : 물품기부게시판 JPA 기능 구현
    
    * feat : MissingPost 실종신고 게시판 CUD 기능 JPA로 구현 완료
    
    * feat : MissingPost 실종신고 게시판 CUD 기능 JPA로 구현 완료
    
    * donationPost, missingPost - swagger, test code 작성중
    
    * WebSecurity 코드 수정
    
    * WebSecurity 코드 수정
    
    * 실종신고 게시판, 물품기부 게시판 swagger 작성 완료

--- CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/donation/command/application/controller/DonationExController.java
+++ CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/donation/command/application/controller/DonationExController.java
@@ -11,6 +11,8 @@ import com.backoven.catdogshelter.domain.donation.command.application.dto.Update
 import com.backoven.catdogshelter.domain.donation.command.application.service.*;
 import com.backoven.catdogshelter.domain.donation.command.domain.aggregate.entity.DonationPost;
 import com.backoven.catdogshelter.domain.user.command.domain.repository.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +24,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/donation-posts")
 @RequiredArgsConstructor
+@Tag(name = "물품기부 게시글 CUD API")
 public class DonationExController {
 
     private final DonationPostCommandService donationPostService;
@@ -32,6 +35,10 @@ public class DonationExController {
 
       /*===============JPA - READ=============== */
 
+    @Operation(
+            summary = "물품기부 게시글 단건 조회",
+            description = "물품기부 게시글을 1개 조회합니다"
+    )
     // 게시글 단건 조회(조회>mybatis에서 다 구현했음)
     @GetMapping("/{id}")
     public ResponseEntity<DonationPostResponse> getPost(@PathVariable Integer id) {
@@ -39,7 +46,10 @@ public class DonationExController {
         return ResponseEntity.ok(new DonationPostResponse(post)); // DTO 변환
     }
 
-
+    @Operation(
+            summary = "물품기부 게시글 전체 조회",
+            description = "품기부 게시글을 전체 조회합니다."
+    )
     // 게시글 전체 조회(조회>mybatis에서 다 구현했음)
     @GetMapping
     public ResponseEntity<List<DonationPostResponse>> getAllPosts() {
@@ -50,7 +60,10 @@ public class DonationExController {
                         .toList()
         );
     }
-
+    @Operation(
+            summary = "물품기부 게시글 작성자 조회",
+            description = "물품기부 게시글 작성자를 조회합니다. 물품기부 게시글 작성은 보호소장만 할 수 있습니다."
+    )
     // 게시글 작성자 (보호소장)ceo_name으로 조회
     @GetMapping("/by-ceo")
     public ResponseEntity<List<DonationPostResponse>> getPostsByCeoName(@RequestParam String ceoName) {
@@ -64,6 +77,10 @@ public class DonationExController {
 
     /* =============== JPA - CUD =============== */
 
+    @Operation(
+            summary = "물품기부 게시글 등록",
+            description = "새로운 물품기부 게시글을 등록합니다."
+    )
     // 게시글 생성 (보호소장만 가능) 제목+내용+파일업로드
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Integer> createDonationPost(
@@ -82,6 +99,10 @@ public class DonationExController {
         return ResponseEntity.ok(postId);
     }
 
+    @Operation(
+            summary = "물품기부 게시글 수정",
+            description = "입력값을 id로 하는 게시글의 내용을 글 작성자만 수정합니다. 파일이 들어오면 기존 파일을 전체 삭제 후 새로 들어온 파일을 등록합니다."
+    )
     //게시글 수정 (본인만 가능) 파일 삭제,첨부 가능
     @PutMapping(value = "/{postId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Void> updateDonationPost(
@@ -100,7 +121,10 @@ public class DonationExController {
         return ResponseEntity.ok().build();
     }
 
-
+    @Operation(
+            summary = "물품기부 게시글 삭제",
+            description = "새입력값을 id로 하는 게시글을 글 작성자만 삭제합니다. 게시글: soft delete"
+    )
     // 게시글 삭제 (작성자 본인만 가능)
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteDonationPost(@PathVariable Integer id,
@@ -109,7 +133,10 @@ public class DonationExController {
         return ResponseEntity.noContent().build();
     }
 
-
+    @Operation(
+            summary = "이미지 다운로드 조회용(테스트)",
+            description = "이미지 다운로드 조회용 테스트 기능입니다."
+    )
     // 이미지 다운로드(조회용)
     @GetMapping("/image/{fileName}")
     public ResponseEntity<byte[]> downloadImage(@PathVariable String fileName) {
@@ -120,7 +147,10 @@ public class DonationExController {
     }
 
 
-
+    @Operation(
+            summary = "물품기부 게시글 좋아요 증가",
+            description = "물품기부 게시글 좋아요 증가처리, 게시글 하나에 한 회원이 중복 증가 불가능"
+    )
     // 좋아요 누르기
     @PostMapping("/{id}/like")
     public ResponseEntity<Void> updateLikeDonationPost(@PathVariable Integer id, @RequestParam Integer userId) {
@@ -128,7 +158,10 @@ public class DonationExController {
         return ResponseEntity.ok().build();
     }
 
-
+    @Operation(
+            summary = "물품기부 게시글 좋아요 감소",
+            description = "물품기부 게시글 좋아요 감소처리, 본인이 좋아요 누른 게시글에만 좋아요 감소처리 가능"
+    )
     // 좋아요 취소
     @DeleteMapping("/{id}/like")
     public ResponseEntity<Void> updateUnLikeDonationPost(@PathVariable Integer id, @RequestParam Integer userId) {
@@ -136,8 +169,10 @@ public class DonationExController {
         return ResponseEntity.noContent().build();
     }
 
-
-
+    @Operation(
+            summary = "물품기부 게시글에 댓글 등록",
+            description = "새로운 물품기부 게시글에 댓글을 등록합니다."
+    )
     //특정 게시글에 댓글 작성
     @PostMapping("/{id}/comments")
     public ResponseEntity<Integer> createDonationPostComment(@PathVariable Integer id,
@@ -147,7 +182,10 @@ public class DonationExController {
         // 댓글 저장 후 생성된 댓글 ID 반환
     }
 
-
+    @Operation(
+            summary = "물품기부 댓글 수정",
+            description = "물품기부 게시글에 작성되어 있는 댓글을 댓글 작성자 본인만 수정합니다."
+    )
     // 댓글 수정 (작성자 본인만 가능)
     @PutMapping("/comments/{id}")
     public ResponseEntity<Void> updateDonationPostComment(@PathVariable Integer id,
@@ -158,7 +196,10 @@ public class DonationExController {
     }
 
 
-
+    @Operation(
+            summary = "물품기부 댓글 삭제",
+            description = "물품기부 게시글에 작성되어 있는 댓글을 댓글 작성자 본인만 삭제가능합니다. 댓글 삭제 : soft delete"
+    )
     // 댓글 삭제 (작성자 본인만 가능)
     @DeleteMapping("/comments/{id}")
     public ResponseEntity<Void> deleteDonationPostComment(@PathVariable Integer id,
@@ -167,7 +208,10 @@ public class DonationExController {
         return ResponseEntity.noContent().build();
     }
 
-
+    @Operation(
+            summary = "물품기부 게시글 댓글 신고(일반 사용자)",
+            description = "물품기부 게시글에 등록되어있는 댓글을 신고합니다. 중복신고는 불가능합니다."
+    )
     // 일반 사용자 댓글 신고
     @PostMapping("/{commentId}/report/user")
     public ResponseEntity<Void> createReportDonationPostCommentByUser(@PathVariable Integer commentId,
@@ -181,6 +225,10 @@ public class DonationExController {
         return ResponseEntity.ok().build();
     }
 
+    @Operation(
+            summary = "물품기부 게시글 댓글 신고(보호소장)",
+            description = "물품기부 게시글에 등록되어있는 댓글을 신고합니다. 중복신고는 불가능합니다."
+    )
     // 보호소장 댓글 신고
     @PostMapping("/{commentId}/report/head")
     public ResponseEntity<Void> createReportDonationPostCommentByHead(@PathVariable Integer commentId,
