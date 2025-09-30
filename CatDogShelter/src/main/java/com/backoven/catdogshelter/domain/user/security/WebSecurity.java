5b873637190deb0a49e133e54c69d70ed7091fe7
haenin <gpdnjsqhdld@naver.com>
2025-09-30 09:09:05 +0900

    devlop 수정

--- CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/user/security/WebSecurity.java
+++ CatDogShelter/src/main/java/com/backoven/catdogshelter/domain/user/security/WebSecurity.java
@@ -59,6 +59,15 @@ public class WebSecurity {
                         .requestMatchers(HttpMethod.GET, "/catdogshelter/user/**").authenticated() // 조회는 인증 필요
                         .requestMatchers(HttpMethod.PUT, "/catdogshelter/user/**").authenticated() // 수정도 인증 필요
                         .requestMatchers("/catdogshelter/admin/**").permitAll()
+                        .requestMatchers("/catdogshelter/**").permitAll()
+                        .requestMatchers("/donation-posts/**").permitAll() // 인증 없이 접근 가능
+                        .requestMatchers("/missing-posts/**").permitAll()// 인증 없이 접근 가능
+                        .requestMatchers(
+                                "/missing-posts/comments/**/report/**",
+                                "/donation-posts/comments/**/report/**",
+                                "/missing-posts/**/report",
+                                "/donation-posts/**/report").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll() // Swagger 허용
                         .anyRequest().authenticated())
                 // 세션을 안쓰겟다
                 /* 설명. Session 방식이 아닌 JWT Token 방식을 사용하겠다. */
